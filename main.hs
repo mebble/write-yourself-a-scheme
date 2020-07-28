6c1f49b8c797cffdb40519620147b92fdc7d1b54
Neil Leeson Syiemlieh <neilsyiemlieh@gmail.com>
2020-07-29 00:15:57 +0530

    chapter one and exercises

--- main.hs
+++ main.hs
@@ -5,5 +5,19 @@ main :: IO ()
 main = do
     args <- getArgs
     case args of
-        [] -> putStrLn ("Please give me a command line argument")
-        _ -> putStrLn ("Hello, " ++ args !! 0)
+        [] -> putStrLn "Please give me a command line argument"
+        cmd:xs -> case cmd of
+            "add" -> case xs of
+                x:y:ys -> putStrLn . show $ (read x :: Int) + (read y :: Int)
+                x:[] -> putStrLn "One more number!"
+                [] -> putStrLn "Give me two numbers"
+            "sub" -> case xs of
+                x:y:ys -> putStrLn . show $ (read x :: Int) - (read y :: Int)
+                x:[] -> putStrLn "One more number!"
+                [] -> putStrLn "Give me two numbers"
+            "greet" -> do
+                putStrLn "\"Hi, I'm main.hs. What's your name?\""  -- same as >>
+                name <- getLine                                    -- same as >>=
+                putStrLn $ "\"Hi, " ++ name ++ "!\""
+            _ -> putStrLn "Unknown command"
+
