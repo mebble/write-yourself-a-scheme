60e528d8989a7adc2972005f89ea5f78b45540ea
Neil Leeson Syiemlieh <neilsyiemlieh@gmail.com>
2020-07-29 21:01:36 +0530

    add notes

--- main.hs
+++ main.hs
@@ -11,12 +11,13 @@ main = do
                 putStrLn "\"Hi, I'm main.hs. What's your name?\""  -- same as >>
                 name <- getLine                                    -- same as >>=
                 putStrLn $ "\"Hi, " ++ name ++ "!\""
-            _ | cmd == "add" || cmd == "sub" -> case xs of  -- Using guards with case pattern matching: https://stackoverflow.com/a/40836465 and https://wiki.haskell.org/Let_vs._Where
+            _ | cmd == "add" || cmd == "sub" -> case xs of  -- Using guards in case pattern matching: https://stackoverflow.com/a/40836465 ("Guards are simply constraints for patterns") and https://wiki.haskell.org/Let_vs._Where
                     [] -> putStrLn "Give me two numbers"
                     x:[] -> putStrLn "One more number!"
-                    x:y:ys -> putStrLn . show $ op x y where
+                    x:y:ys -> putStrLn . show $ op x y where  -- Function with guards inside a where: https://stackoverflow.com/questions/13327374/haskell-defining-a-function-with-guards-inside-a-where
                         op m n
                             | cmd == "add" = (read x :: Int) + (read y :: Int)
                             | cmd == "sub" = (read x :: Int) - (read y :: Int)
               | otherwise -> putStrLn "Unknown command"
 
+-- Guard in "let expression": https://stackoverflow.com/a/59778615
