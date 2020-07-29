e8377601e99dae0b616285dbb52539c0995a9698
Neil Leeson Syiemlieh <neilsyiemlieh@gmail.com>
2020-07-29 20:51:47 +0530

    ch1 exercise v2

--- main.hs
+++ main.hs
@@ -7,17 +7,16 @@ main = do
     case args of
         [] -> putStrLn "Please give me a command line argument"
         cmd:xs -> case cmd of
-            "add" -> case xs of
-                x:y:ys -> putStrLn . show $ (read x :: Int) + (read y :: Int)
-                x:[] -> putStrLn "One more number!"
-                [] -> putStrLn "Give me two numbers"
-            "sub" -> case xs of
-                x:y:ys -> putStrLn . show $ (read x :: Int) - (read y :: Int)
-                x:[] -> putStrLn "One more number!"
-                [] -> putStrLn "Give me two numbers"
             "greet" -> do
                 putStrLn "\"Hi, I'm main.hs. What's your name?\""  -- same as >>
                 name <- getLine                                    -- same as >>=
                 putStrLn $ "\"Hi, " ++ name ++ "!\""
-            _ -> putStrLn "Unknown command"
+            _ | cmd == "add" || cmd == "sub" -> case xs of  -- Using guards with case pattern matching: https://stackoverflow.com/a/40836465 and https://wiki.haskell.org/Let_vs._Where
+                    [] -> putStrLn "Give me two numbers"
+                    x:[] -> putStrLn "One more number!"
+                    x:y:ys -> putStrLn . show $ op x y where
+                        op m n
+                            | cmd == "add" = (read x :: Int) + (read y :: Int)
+                            | cmd == "sub" = (read x :: Int) - (read y :: Int)
+              | otherwise -> putStrLn "Unknown command"
 
